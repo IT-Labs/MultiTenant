e0f555157faabe206b823fb90dac9c0d5fc2d1b4
kostadin.kadiev <kostadin.kadiev@it-labs.com>
2020-04-15 15:34:46 +0200

    Adding method for Tenant Configuration Adding method for adding Tasks

---
+++ ItLabs.MultiTenant.Api/Controllers/TenantsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItLabs.MultiTenant.Api.Controllers
+{
+    /// <summary>
+    /// Controller used to showcase the different tasks per tenant
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class TenantsController : ControllerBase
+    {
+        private readonly TasksDbContext _dbContext;
+
+        public TenantsController(TasksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public string ConfigureDatabase()
+        {
+             _dbContext.Database.Migrate();
+            return "Tenant Database has been successfully created and migrated to latest schema version";
+        }
+    }
+}
